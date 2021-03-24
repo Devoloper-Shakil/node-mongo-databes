5800a94d42f72e3eb31053c56ee3bf63685d073a
shakil hasan <shshakilhasan69@gmail.com.com>
2021-03-24 12:47:01 -0700

    databes done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 
 const MongoClient = require('mongodb').MongoClient; 
+const ObjectId=require('mongodb').ObjectId;
  const password ="ZdZM4mkzcqv5GpF";
 
  
@@ -22,17 +23,55 @@ app.get('/', (req, res)=> {
 
 client.connect(err => {
   const collection = client.db("shakil").collection("product");
+
+ app.get("/products" ,(req, res)=>{
+   collection.find({})
+   .toArray((err,document)=>{
+      res.send(document);
+   })
+ })
+
+
   app.post ("/product", (req,res)=>{
       const product=(req.body);
         collection.insertOne(product )
         .then (resulet=>{
-            console.log("product ad ad ad ad ");
-            res.send("succseful add your product");
+            // console.log("product ad ad ad ad ");
+            res.redirect('/')
+            // res.send("succseful add your product");
         })
-  }) 
+  })
   
+  app.get('/produc/:id',(req,res)=>{
+    collection.find({_id:ObjectId (req.params.id)})
+    .toArray((err,document)=>{
+     res.send(document[0]);
+    })
+  })
+
+  app.patch('/update/:id', (req,res)=>{
+    collection.updateOne({_id:ObjectId (req.params.id)},
+    {
+      $set:{price:req.body.price , quantritry:req.body.quantritry}
+    })
+    .then(result=>{
+      res.send(result.modifiedCount>0);
+    //  console.log(result)
+      
+    })
+  })
+
+  app.delete("/delete/:id", (req , res)=>{
+    collection.deleteOne({_id:ObjectId (req.params.id)})
+    .then(result=>{
+      res.send(result.deletedCount>0);
+      // console.log(result);
+    })
+  
+  });
 
 });
+
 
 
 app.listen(3000)
